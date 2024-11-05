6984aa3c9dd1f72a3b005bf4f602cf8f156ca62b
opedrosiqueira <opedrosiqueira@gmail.com>
2024-11-05 14:12:13 -0400

    novo projeto pnpm

---
+++ cap02/input-output.js
@@ -0,0 +1,10 @@
+import scanf from 'scanf';
+
+console.log('Please input your name');
+let name = scanf('%s');
+
+console.log('Please input your age');
+let age = scanf('%d');
+
+console.log('your name', name, 'is a', typeof name);
+console.log('your age', age, 'is a', typeof age);
